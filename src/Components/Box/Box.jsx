6e30099a71a5a0ed82ea225cb178c2418f3a86ec
user <123456@gmail.com>
2024-04-09 18:12:44 +0800

    第一次修改

--- src/Components/Box/Box.jsx
+++ src/Components/Box/Box.jsx
@@ -1,30 +1,26 @@
-import React,{ createContext, useContext } from 'react'
-import './Box.css'
+import React, { createContext, useContext } from "react"
+import "./Box.css"
 
 export default function Box(props) {
-    
-    // const {handleClick} = React.useContext(Context)
-    const {event} = props
-    const publish = (e)=>{
+    const { event } = props
+    const publish = e => {
         event.current.handleClick(e)
     }
 
-    const chessbox = () => {
-        const box = []
-        for (let row = 0; row < 10; row++) {
-            const rowBox = []
-            for (let col = 0; col < 9; col++) {
-                const left = col* 80
-                const top = row *80
-                rowBox.push(<div onClick={publish} className="box-list" key={`${row}-${col}`} style={{ left: `${left-40}px`, top: `${top-40}px` }} ></div>)
-            }
-            box.push(
-                <div className="box-row" key={row}>
-                    {rowBox}
-                </div>
-            )
-        }
-        return box
-    }
-    return <div className="box">{chessbox()}</div>
+    const box = []
+    Array.from({ length: 9 }).map((_, indexR) => {
+        const rowBox = []
+        Array.from({ length: 8 }).map((_, indexL) => {
+            const left = indexL * 80
+            const top = indexR * 80
+            rowBox.push(<div onClick={publish} className="box-list" key={`${indexR}-${indexL}`} style={{ left: `${left - 40}px`, top: `${top - 40}px` }}></div>)
+        })
+        box.push(
+            <div className="box-row" key={indexR}>
+                {rowBox}
+            </div>
+        )
+    })
+
+    return <div className="box">{box}</div>
 }
